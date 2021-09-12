3143832bea5e4af6f938bd5a9c5c272bf3139d6d
Adam <adam.bensalah@yahoo.fr>
2021-09-12 10:56:12 +0100

    Started code snippets

--- scraper/chalicelib/CovApi.py
+++ scraper/chalicelib/CovApi.py
@@ -1,10 +1,23 @@
 import logging
+import os
+
+import requests
+from urllib.parse import urlparse
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+# from urlparse import urlparse  # Python 2
+parsed_uri = urlparse(os.environ.get("COV_API_URL"))
+host = '{uri.netloc}'.format(uri=parsed_uri)
+
+class CovApiError(Exception):
+    pass
 
 class CovApi:
+    #TODO: Insert into database and get from database
+    def __init__(self, api_endpoint: str):
+        self.api_endpoint = api_endpoint
     def get_facebook_groups(self):
         return ["1493465070746580"]
 
@@ -15,3 +28,27 @@ class CovApi:
             logger.info((
                 row['id'], row['text'], row['Author'], row['Author_profile'], row['Post_time'], row['Extract_time'],
                 row['Post_link'], fb_group))
+            raw = {
+                'id': row['id']
+            }
+            try:
+                self.insert_raw(raw)
+            except CovApiError:
+
+
+    def insert_raw(self, raw):
+
+        from aws_requests_auth.boto_utils import BotoAWSRequestsAuth
+        auth = BotoAWSRequestsAuth(aws_host=host,
+                                   aws_region='eu-west-1',
+                                   aws_service='execute-api')
+        structured_endpoint = f"{self.api_endpoint}/raw"
+        # Inserting new structured entry in db
+        logger.info(f"Inserting {raw} to {structured_endpoint}")
+        res = requests.post(structured_endpoint, json=raw, auth=auth)
+        if res.status_code == 200:
+            logger.info("Successfully inserted row to Cov API")
+            logger.info(res.text)
+        else:
+            logger.info(f"Couldn't insert row to Cov API, {res.text}")
+            raise CovApiError(res.text)
